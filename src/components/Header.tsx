d8225f409e1947ea9651dc7f665ac24be969c716
seungjinhan <hanblues@gmail.com>
2024-04-24 17:02:38 +0800

    dart, light

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,12 @@ import {
   Button,
   HStack,
   IconButton,
+  LightMode,
+  useColorMode,
+  useColorModeValue,
   useDisclosure,
 } from "@chakra-ui/react";
-import { FaAirbnb, FaMoon } from "react-icons/fa";
+import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 
@@ -20,6 +23,10 @@ export default function Header() {
     onClose: onSignUpClose,
     onOpen: onSignUpOpen,
   } = useDisclosure();
+
+  const { toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.200");
+  const Icon = useColorModeValue(FaMoon, FaSun);
   return (
     <>
       <HStack
@@ -28,19 +35,22 @@ export default function Header() {
         px={10}
         borderBottomWidth={1}
       >
-        <Box color={"red.500"}>
+        <Box color={logoColor}>
           <FaAirbnb size={48} />
         </Box>
         <HStack spacing={2}>
           <IconButton
+            onClick={toggleColorMode}
             variant={"Ghost"}
             aria-label='Toggle dark mode'
-            icon={<FaMoon />}
+            icon={<Icon />}
           />
           <Button onClick={onLoginOpen}>Log in</Button>
-          <Button onClick={onSignUpOpen} colorScheme='red'>
-            Sign up
-          </Button>
+          <LightMode>
+            <Button onClick={onSignUpOpen} colorScheme='red'>
+              Sign up
+            </Button>
+          </LightMode>
         </HStack>
       </HStack>
       <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
