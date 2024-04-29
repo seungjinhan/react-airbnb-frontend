c3e2bcd37b5fdeb4468174118cbd6726948a6ff2
seungjinhan <hanblues@gmail.com>
2024-04-29 11:42:18 +0800

    form login, react hook form

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -10,24 +10,39 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Text,
   VStack,
 } from "@chakra-ui/react";
 import { FaLock, FaUserNinja } from "react-icons/fa";
 import SocialLogin from "./SocialLogin";
+import { useForm } from "react-hook-form";
 
 interface LoginModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+interface IForm {
+  username: string;
+  password: string;
+}
+
 export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit = (data: IForm) => {};
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Log In</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
+        <ModalBody as='form' onSubmit={handleSubmit(onSubmit)}>
           <VStack>
             <InputGroup>
               <InputLeftElement
@@ -37,7 +52,12 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
                   </Box>
                 }
               />
-              <Input placeholder='Username' variant={"filled"} />
+              <Input
+                isInvalid={Boolean(errors.username?.message)}
+                {...register("username", { required: "Please input username" })}
+                placeholder='Username'
+                variant={"filled"}
+              />
             </InputGroup>
             <InputGroup>
               <InputLeftElement
@@ -47,9 +67,17 @@ export default function LoginModal({ isOpen, onClose }: LoginModalProps) {
                   </Box>
                 }
               />
-              <Input placeholder='Password' variant={"filled"} />
+              <Input
+                isInvalid={Boolean(errors.password?.message)}
+                {...register("password", {
+                  required: "Please input passworld",
+                })}
+                placeholder='Password'
+                variant={"filled"}
+                type='password'
+              />
             </InputGroup>
-            <Button mt={4} w='100%' colorScheme='red'>
+            <Button mt={4} w='100%' colorScheme='red' type='submit'>
               Log in
             </Button>
           </VStack>
